93efcdc628ab6734716289b2c781fb37f958af1a
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-02 19:13:49 -0500

    Angular

---
+++ Componentes/ComponentAnatomy.ts
@@ -0,0 +1,23 @@
+// Component Anatomy (Anatomía de un Componente)
+
+// Un componente Angular tiene 3 partes clave:
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//     selector: 'app-ejemplo',
+//     templateUrl: './ejemplo.component.html',
+//     styleUrls: ['./ejemplo.component.css']
+// })
+// export class EjemploComponent {
+//     titulo = 'Hola angular';
+// }
+
+// Partes:
+
+// @Component: Decorador que define la metadata.
+// selector: Etiqueta HTML personalizada (<app-ejemplo).
+// templateUrl: Ruta al archivo HTML.
+// styleUrls: Estilos CSS
+// class: Lógica del componente
+
