645011aed186992c7d2580cbb8d6f3e2f9f932ff
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-09 21:21:50 -0500

    Angular Agenda de Contactos

--- AgendaContactos/src/app/components/header/header.ts
+++ AgendaContactos/src/app/components/header/header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Al importarlo me permite llamar a cada una de las paginas sin cargar.
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
--- AgendaContactos/src/app/pages/home/home.ts
+++ AgendaContactos/src/app/pages/home/home.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface Contacto {
+  name: string;
+  phone: number | null;
+  email: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  contacto: Contacto = {
+    name: '',
+    phone: null,
+    email: ''
+  };
 
+  contactos: Contacto[] = [];
+
+  guardarContacto() {
+    if (this.contacto.name && this.contacto.phone && this.contacto.email.trim()) {
+
+      // Agregar el contacto a la lista
+      this.contactos.push({ ...this.contacto });
+      // Limpiar el formulario
+      this.contacto = { name: '', phone: 0, email: '' };
+
+    }
+  }
 }
