a856bfdbed068e346621f7202c83a78ee466569c
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-15 16:33:42 -0500

    Angular y Tailwind Agenda de Contactos

--- AgendaContactos/src/app/app.routes.ts
+++ AgendaContactos/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home'
-import { About } from './pages/about/about';
-
+import { Footer } from './components/footer/footer';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: "about", component: About },
     { path: '**', redirectTo: '' } // Redirecciona a la ruta raíz
 ];
 
