e92f448689f61e62204c157a1b18dbe2b7c352be
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-15 19:50:44 -0500

    Angular Tailwind Agenda de Contactos

--- AgendaContactos/src/app/app.routes.ts
+++ AgendaContactos/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home'
-import { Footer } from './components/footer/footer';
 
 export const routes: Routes = [
     { path: '', component: Home },
